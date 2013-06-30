ce2397627e15c69324dde4c38a9285ae1746290c
servetto.matias@gmail.com <servetto.matias@gmail.com@5ae81eff-20de-bdc6-67a4-fe779ac0b9a5>
2013-06-30 23:11:20 +0000

    Option menu: settings (SettingsActivity Template)

--- trunk/AppSample/src/com/example/appsample/FullscreenActivity.java
+++ trunk/AppSample/src/com/example/appsample/FullscreenActivity.java
@@ -4,22 +4,37 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
-public class FullscreenActivity extends Activity {
+public class FullscreenActivity extends Activity{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fullscreen);
     }
 
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.fullscreen, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+	    switch (item.getItemId())
+	    {
+	    case R.id.action_settings:
+	        startActivity(new Intent(this, SettingsActivity.class));
+	        return true;
+	    default:
+	        return super.onOptionsItemSelected(item);
+	    }
     }
     
     public void findNearToiletsTapped(View view)
@@ -28,4 +43,9 @@ public class FullscreenActivity extends Activity {
     	startActivity(intent);
     }
     
+    public void settingsMenuItemTapped(View view)
+    {
+    	Intent intent = new Intent(this, SettingsActivity.class);
+    	startActivity(intent);
+    }
 }
