443c7b429307a800af17a55051c1b91f35a03975
damian.schenkelman@gmail.com <damian.schenkelman@gmail.com@5ae81eff-20de-bdc6-67a4-fe779ac0b9a5>
2013-07-13 15:46:39 +0000

    # Ability to retrieve toilets near location done

---
+++ trunk/src/SocialToilet.Api/SocialToilet.Api/Helpers/ToiletExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace SocialToilet.Api.Helpers
+{
+    using SocialToilet.Api.Models;
+    using SocialToilet.Api.ViewModels;
+
+    public static class ToiletExtensions
+    {
+        public static ToiletViewModel ToViewModel(this Toilet toilet)
+        {
+            return new ToiletViewModel()
+                       {
+                           Address = toilet.Address,
+                           Id = toilet.Id,
+                           Location = new Location 
+                           { 
+                               Latitude = toilet.Location.Latitude ?? 0,
+                               Longitude = toilet.Location.Longitude ?? 0
+                           }
+                       };
+        }
+    }
+}
---
+++ trunk/src/SocialToilet.Api/SocialToilet.Api/ViewModels/ToiletViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace SocialToilet.Api.ViewModels
+{
+    using System;
+
+    using SocialToilet.Api.Models;
+
+    public class ToiletViewModel
+    {
+        public Guid Id { get; set; }
+
+        public Location Location { get; set; }
+
+        public string Address { get; set; }
+    }
+}
