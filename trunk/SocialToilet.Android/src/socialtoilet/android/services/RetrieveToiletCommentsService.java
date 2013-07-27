1aba412591e7fb2674143e5f7ae3aa6b4df7b334
servetto.matias@gmail.com <servetto.matias@gmail.com@5ae81eff-20de-bdc6-67a4-fe779ac0b9a5>
2013-07-27 19:19:34 +0000

    The comments activity Some detail toilet activity error fixed

---
+++ trunk/SocialToilet.Android/src/socialtoilet/android/services/RetrieveToiletCommentsService.java
@@ -0,0 +1,17 @@
+package socialtoilet.android.services;
+
+import java.util.UUID;
+
+public class RetrieveToiletCommentsService extends GetService implements
+		IRetrieveToiletCommentsService
+{
+
+	@Override
+	public void retrieveToiletComments(UUID toiletId,
+			IRetrieveToiletCommentsServiceDelegate delegate)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
