5c41034c7160087a75e48ff86ea925f5725a41f1
servetto.matias@gmail.com <servetto.matias@gmail.com@5ae81eff-20de-bdc6-67a4-fe779ac0b9a5>
2013-07-29 02:26:10 +0000

    detalles visuales

--- SocialToilet.Android/src/socialtoilet/android/activities/AddToiletActivity.java
+++ SocialToilet.Android/src/socialtoilet/android/activities/AddToiletActivity.java
@@ -61,13 +61,6 @@ public class AddToiletActivity extends Activity implements IAddToiletServiceDele
 		switch (item.getItemId())
 		{
 		case android.R.id.home:
-			// This ID represents the Home or Up button. In the case of this
-			// activity, the Up button is shown. Use NavUtils to allow users
-			// to navigate up one level in the application structure. For
-			// more details, see the Navigation pattern on Android Design:
-			//
-			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
-			//
 			NavUtils.navigateUpFromSameTask(this);
 			return true;
 		}
@@ -104,6 +97,5 @@ public class AddToiletActivity extends Activity implements IAddToiletServiceDele
 			String errorCode)
 	{
 		Toast.makeText(getApplicationContext(), "Error adding toilet", Toast.LENGTH_SHORT).show();
-		
 	}
 }
