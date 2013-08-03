a4d43b5edb035b0ce8db85ebe406180b8a35cba5
servetto.matias@gmail.com <servetto.matias@gmail.com@5ae81eff-20de-bdc6-67a4-fe779ac0b9a5>
2013-08-03 13:46:08 +0000

    buttons on add toilet match width

--- SocialToilet.Android/src/socialtoilet/android/activities/SettingsActivity.java
+++ SocialToilet.Android/src/socialtoilet/android/activities/SettingsActivity.java
@@ -3,24 +3,13 @@ package socialtoilet.android.activities;
 import socialtoilet.android.R;
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
-import android.annotation.TargetApi;
-import android.os.Build;
 
-public class SettingsActivity extends Activity {
-
+public class SettingsActivity extends Activity
+{
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState)
+	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_settings);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.settings, menu);
-		return true;
 	}
 }
