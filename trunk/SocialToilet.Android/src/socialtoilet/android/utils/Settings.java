b59dfd7b21be70532d71f9852ac91395b5102ccb
servetto.matias@gmail.com <servetto.matias@gmail.com@5ae81eff-20de-bdc6-67a4-fe779ac0b9a5>
2013-08-04 20:05:11 +0000

    Save user data in SharePreferences

--- trunk/SocialToilet.Android/src/socialtoilet/android/utils/Settings.java
+++ trunk/SocialToilet.Android/src/socialtoilet/android/utils/Settings.java
@@ -2,31 +2,41 @@ package socialtoilet.android.utils;
 
 import java.util.UUID;
 
+import android.content.SharedPreferences;
+
 import socialtoilet.android.model.LoginUser;
 
 public class Settings
 {
 
-	private static Settings instance;
+    public static final String kAccountUser = "kAccountUser";
+    public static final String kAccountPassword = "kAccountPassword";
+    public static final String kAccountUserId = "kAccountUserId";
+    public static final String kAccountIsSessionOn = "kAccountIsSessionOn";
+    
+    private static Settings instance;
+	private SharedPreferences settings;
 	
 	private boolean sessionOn;
 	private LoginUser user;
 	private int initialRadiusInMeters;
 	private boolean servicesDebugMode;
 	
-	private Settings()
+	private Settings(SharedPreferences settings)
 	{
+		this.settings = settings;
 		sessionOn = false;
-		initialRadiusInMeters = 1000;
+		initialRadiusInMeters = 10000;
 		servicesDebugMode = false;
+	}
+	
+	public static void initialize(SharedPreferences settings)
+	{
+		instance = new Settings(settings);
 	}
 	
 	public static Settings getInstance()
 	{
-		if( null == instance )
-		{
-			instance = new Settings();
-		}
 		return instance;
 	}
 	
@@ -62,19 +72,54 @@ public class Settings
 	
 	public void retrieveUser()
 	{
-		// TODO encontrar una manera para guardar el usuario y contraseña y recuperarlo
-		sessionOn = false;
+		sessionOn = settings.getBoolean(kAccountIsSessionOn, false);
+		if(sessionOn)
+		{
+			String userS, passwordS, userIdS;
+			userS = settings.getString(kAccountUser, "");
+			passwordS = settings.getString(kAccountPassword, "");
+			userIdS = settings.getString(kAccountUserId, "");
+			
+			if(0 == userS.length() || 0 == passwordS.length() || 0 == userIdS.length())
+			{
+				sessionOn = false;
+			}
+			else
+			{
+				this.user = new LoginUser(userS, passwordS);
+				this.user.setUserId(UUID.fromString(userIdS));
+			}
+		}
 	}
 
 	public void setUser(LoginUser user)
 	{
 		sessionOn = true;
 		this.user = user;
-		// TODO dave user in disk
+		
+		SharedPreferences.Editor editor = settings.edit();
+		editor.putString(kAccountUser, user.getUser());
+		editor.putString(kAccountPassword, user.getPassword());
+		editor.putString(kAccountUserId, user.getUserId().toString());
+		editor.putBoolean(kAccountIsSessionOn, true);
+		editor.commit();
 	}
 
 	public void setAuthUser(LoginUser user)
 	{
 		this.user = user;
+	}
+	
+	public void logoutUser()
+	{
+		sessionOn = false;
+		this.user = null;
+		
+		SharedPreferences.Editor editor = settings.edit();
+		editor.putString(kAccountUser, "");
+		editor.putString(kAccountPassword, "");
+		editor.putString(kAccountUserId, "");
+		editor.putBoolean(kAccountIsSessionOn, false);
+		editor.commit();
 	}
 }
